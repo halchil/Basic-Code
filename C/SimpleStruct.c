6cf25595eac489bce2ce289710d5d164c6022d13
halchil <halzcreate1215@gmail.com>
2024-09-19 23:13:16 +0900

    commit

---
+++ C/SimpleStruct.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// 構造体宣言
+struct SinpleStruct {
+        int number;
+        char letter;
+};
+
+// 構造体の初期化
+struct SinpleStruct example = {
+        42,
+        'A'
+};
+
+int main() {
+        printf("Number: %d\n", example.number);
+        printf("Letter: %c\n", example.letter);
+
+        return 0;
+}
